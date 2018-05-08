8e6e15dd8b2bb35f4dd1262193814133e9faf277
Finn <3guenthe@informatik.uni-hamburg.de>
2018-05-08 09:06:24 +0200

    Fix Python Embed download path

--- buildsystem/scripts/EmbedPython.cmake
+++ buildsystem/scripts/EmbedPython.cmake
@@ -7,9 +7,12 @@ endif()
 include("${buildsystem_dir}/modules/DownloadCache.cmake")
 set(python_embed_zip "python-${py_version}-embed-win32.zip")
 set(python_zip_download_path "${downloads_dir}/${python_embed_zip}")
+STRING(REGEX MATCH "[0-9]\\.[0-9]\\.[0-9]" python_download_dir ${py_version})
 # TODO: disable downloading for CI builds. <Need CI first>
+# FIXME: Tries to download nonexistant https://www.python.org/ftp/python/3.7.0b4/python-3.7.0b4-embed-win32.zip
+# Should be .../python/3.7.0/
 download_cache(
-	"https://www.python.org/ftp/python/${py_version}/${python_embed_zip}"
+	"https://www.python.org/ftp/python/${python_download_dir}/${python_embed_zip}"
 	"${python_zip_download_path}"
 )
 
